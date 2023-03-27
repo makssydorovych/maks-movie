c88da1d2d0c44ec85c210aa54f30c0acc724c3d5
Maksym <maksymsydorovych@gmail.com>
2023-03-27 12:17:15 +0200

    NextAuth with bcrypt JWT secret

--- pages/api/[...nextauth].ts
+++ pages/api/[...nextauth].ts
@@ -1,7 +1,8 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import prismadb from "@/lib/prismadb";
-import { compare } from 'bcrypt';
+import {compare} from 'bcrypt';
+
 
 export default NextAuth({
     providers: [
@@ -15,23 +16,40 @@ export default NextAuth({
                 },
                 password: {
                     label: 'Password',
-                    type: 'Password',
+                    type: 'password'
                 }
             },
-            async authorize(credentials) {
+            async authorize(credentials ) {
                 if (!credentials?.email || !credentials?.password) {
                     throw new Error('Email and password required');
                 }
-                const user = await prismadb.user.findUnique({
-                    where: {
+
+                const user = await prismadb.user.findUnique({ where: {
                         email: credentials.email
-                    }
-                });
+                    }});
+
                 if (!user || !user.hashedPassword) {
                     throw new Error('Email does not exist');
                 }
+
                 const isCorrectPassword = await compare(credentials.password, user.hashedPassword);
+
+                if (!isCorrectPassword) {
+                    throw new Error('Incorrect password');
+                }
+
+                return user;
             }
         })
-    ]
+    ],
+    pages: {
+        signIn: '/auth'
+    },
+    debug: process.env.NODE_ENV === 'development',
+    // adapter: PrismaAdapter(prismadb),
+    session: { strategy: 'jwt' },
+    jwt: {
+        secret: process.env.NEXTAUTH_JWT_SECRET,
+    },
+    secret: process.env.NEXTAUTH_SECRET
 })
