7125cc13fc43d2d1ee684daa193e4bf2698b232d
Maksym <maksymsydorovych@gmail.com>
2023-03-27 13:40:15 +0200

    index.ts ServerSideProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,42 @@
-export default function Home() {
-  return (
-    <>
-      <h1>MaKs-moVie</h1>
-    </>
-  )
+import React from 'react';
+import { NextPageContext } from 'next';
+import { getSession } from 'next-auth/react';
+
+
+
+export async function getServerSideProps(context: NextPageContext) {
+    const session = await getSession(context);
+
+    if (!session) {
+        return {
+            redirect: {
+                destination: '/auth',
+                permanent: false,
+            }
+        }
+    }
+
+    return {
+        props: {}
+    }
 }
+
+const Home = () => {
+    const { data: movies = [] } = useMovieList();
+    const { data: favorites = [] } = useFavorites();
+    const {isOpen, closeModal} = useInfoModalStore();
+
+    return (
+        <>
+            <InfoModal visible={isOpen} onClose={closeModal} />
+            <Navbar />
+            <Billboard />
+            <div className="pb-40">
+                <MovieList title="Trending Now" data={movies} />
+                <MovieList title="My List" data={favorites} />
+            </div>
+        </>
+    )
+}
+
+export default Home;
